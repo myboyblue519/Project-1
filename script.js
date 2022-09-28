2d301ad80e2c0c6e110b37acd1e5011573f76717
mattvl <mvamlux@gmail.com>
2022-09-27 20:16:45 -0400

    fixed carousel and hamburger

--- script.js
+++ script.js
@@ -24,6 +24,5 @@ const toggleButton = document.getElementsByClassName("toggleButton")[0]
 const navBarLinks = document.getElementsByClassName("navBarLinks")[0]
 
 toggleButton.addEventListener ('click', () => {
-    // navBarLinks.classList.toggle('active');
-    console.log('hello world')
+    navBarLinks.classList.toggle('active');
 });
